e206811aaea05d05ef4864475f7238bfb0623284
wudaxun <522864637@qq.com>
2019-06-18 15:03:53 +0800

    redux复习

--- day07/src/index.js
+++ day07/src/index.js
@@ -1,16 +1,13 @@
-import React from 'react'; // 必须得引入，因为ReactDOM依赖于此
-import ReactDOM from 'react-dom'; // 入口文件处必须引入，因为需要渲染DOM节点的内容
-import App from '@/components/App'; // 引入一个App.js的组件
-import store from './store';
-
-// 给页面中id为root的标签渲染了一个 App 的组件
-// 在react中组件的首字母必须大写，因为如果写的是小写，第一可能与HTML标签冲突
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '@/components/App';
+import store from './store'
 function renderFn () {
   ReactDOM.render(
     <App />,
     document.getElementById('root')
   )
 }
-renderFn()
 
+renderFn();
 store.subscribe(renderFn)
--- day07/src/store.js
+++ day07/src/store.js
@@ -1,20 +1,29 @@
 import { createStore } from 'redux';
+
 const reducer = (state = {
-  bannerlist:[],
-  prolist:[]
+  bannerlist: [],
+  prolist: []
 }, action) => {
-  const { type, data } = action // 触发action靠的是type，data为数据
+  const { type, data } = action;
   switch (type) {
-    case 'changeBannerlist':
-      state.bannerlist = data;
+    case 'changeBannerList':
+      state = {
+        bannerlist: data,
+        prolist: state.prolist
+      }
       return state;
-    case 'changeProlist':
-      state.prolist = data;
+    case 'changeProList':
+      state = {
+        bannerlist: state.bannerlist,
+        prolist: data
+      }
       return state;
+  
     default:
-      return state;
+      return state
   }
 }
-const store = createStore(reducer)
 
-export default store;
+const store = createStore(reducer);
+
+export default store
